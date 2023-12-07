3ca6fd9af8511b05165fcbc0ff68c737001e3c16
hisaki <alltimelow2011@gmail.com>
2023-12-07 21:42:51 +0900

    ログイン後に天気情報を取得

--- app/controllers/wheathers_controller.rb
+++ app/controllers/wheathers_controller.rb
@@ -1,4 +1,20 @@
+require 'json'
+require 'net/https'
+require 'uri'
+
 class WheathersController < ApplicationController
   def index
+    api_key = ENV['OPEN_WEATHER_API']
+    city = 'Tokyo'
+
+    uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}&lang=ja&units=metric")
+    response = Net::HTTP.get(uri)
+    @weather_data = JSON.parse(response)
+    if @weather_data.present?
+      @weather_description = @weather_data["weather"][0]["description"]
+      @weather_temp = @weather_data["main"]["temp"]
+    else
+      @error_message = "取得できませんでした"
+    end
   end
 end
