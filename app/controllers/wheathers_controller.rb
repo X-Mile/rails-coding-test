35aa87003882f731f13bd2dddeec94434512b334
hisaki <alltimelow2011@gmail.com>
2023-12-08 20:44:09 +0900

    coderabbitaiの指摘修正

--- app/controllers/wheathers_controller.rb
+++ app/controllers/wheathers_controller.rb
@@ -7,14 +7,26 @@ class WheathersController < ApplicationController
     api_key = ENV['OPEN_WEATHER_API']
     city = 'Tokyo'
 
-    uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}&lang=ja&units=metric")
-    response = Net::HTTP.get(uri)
-    @weather_data = JSON.parse(response)
-    if @weather_data.present?
-      @weather_description = @weather_data["weather"][0]["description"]
-      @weather_temp = @weather_data["main"]["temp"]
-    else
-      @error_message = "取得できませんでした"
+    begin
+      uri = URI("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}&lang=ja&units=metric")
+      response = Net::HTTP.get(uri)
+      
+      if response.code == "200"
+        response_body = response.body
+        @weather_data = JSON.parse(response_body)
+        
+        if @weather_data.present? && @weather_data['main'].present?
+          @weather_description = @weather_data["weather"][0]["description"]
+          @weather_temp = @weather_data["main"]["temp"]
+        else
+          @error_message = "必要なデータを取得できませんでした"
+        end
+      else
+        @error_message = = "無効なレスポンスです: #{response.code}"
+    rescue JSON::ParserError
+      @error_message = "データの解析に失敗しました"
+    rescue => e
+      @error_message = "データの取得中にエラーが発生しました: #{e.message}"
     end
   end
 end
